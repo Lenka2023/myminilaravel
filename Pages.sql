56acb82ed5d2e9a701667b3d41ca38a8dd86bebb
Elena <Lenka20231@gmail.com>
2018-09-08 22:19:11 +0300

    add new dump

--- Pages.sql
+++ Pages.sql
@@ -1,76 +1,74 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
--- https://www.phpmyadmin.net/
+-- version 4.2.11
+-- http://www.phpmyadmin.net
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Мар 25 2018 г., 15:34
--- Версия сервера: 5.6.38
--- Версия PHP: 5.5.38
+-- Хост: 127.0.0.1
+-- Время создания: Сен 08 2018 г., 21:17
+-- Версия сервера: 5.6.21
+-- Версия PHP: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- База данных: `Pages`
+-- База данных: `app`
 --
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `Pages`
+-- Структура таблицы `pages`
 --
 
-CREATE TABLE `Pages` (
-  `id` int(10) NOT NULL,
-  `text` varchar(65) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `pages` (
+`id` int(10) unsigned NOT NULL,
+  `text` varchar(10000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Дамп данных таблицы `Pages`
+-- Дамп данных таблицы `pages`
 --
 
-INSERT INTO `Pages` (`id`, `text`) VALUES
-(1, '1'),
-(2, '2'),
-(3, '12'),
-(4, '47'),
-(5, '15'),
-(6, 'дл'),
-(7, 'дл'),
-(8, 'дл<div class=\"commands\"><pre>ро</pre></div>'),
-(9, 'дл<div class=\"commands\"><pre>ро</pre></div>'),
-(10, 'ЛОПО<div class=\"commands\"><pre>РП</pre></div>');
+INSERT INTO `pages` (`id`, `text`, `created_at`, `updated_at`) VALUES
+(1, 'luhiu<div class="commands"><pre>yhgyg</pre></div>', NULL, NULL),
+(2, 'plop<span class="strong">koko</span>', NULL, NULL),
+(3, 'kjh<span class="strong">iy</span>', NULL, NULL),
+(4, 'kjh<span class="strong">iy</span>', NULL, NULL),
+(5, ',j<span class="strong">jh</span>,j<span class="strong">j</span>', NULL, NULL),
+(6, 'ku<span class="strong">giyg</span>ku<span class="strong">g</span><span class="strong">y</span>', NULL, NULL),
+(7, 'iuy<span class="strong">iy</span>', NULL, NULL),
+(8, 'jhjh<span class="strong">gjh</span>', NULL, NULL),
+(9, 'uhiug<span class="strong">uiy</span>', NULL, NULL),
+(10, 'hghf<div class="commands"><pre>tft</pre></div>', NULL, NULL),
+(11, '12<span class="strong">34</span>', NULL, NULL);
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
--- Индексы таблицы `Pages`
+-- Индексы таблицы `pages`
 --
-ALTER TABLE `Pages`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
+ALTER TABLE `pages`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
--- AUTO_INCREMENT для таблицы `Pages`
+-- AUTO_INCREMENT для таблицы `pages`
 --
-ALTER TABLE `Pages`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-COMMIT;
-
+ALTER TABLE `pages`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
